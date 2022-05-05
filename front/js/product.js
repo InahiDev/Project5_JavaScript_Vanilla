aee0772628216979660973a76220fadf7f1600da
Gabriel Delaigue <gabriel.delaigue@gmail.com>
2022-05-05 17:18:41 +0200

    function cart fonctionnelle, message erreur wip

--- front/js/product.js
+++ front/js/product.js
@@ -5,7 +5,7 @@ function getUrlParameterId() {
   return id //Renvoyer uniquement l'id
 }
 
-const id = getUrlParameterId() //Stocker l'id dans une constante au travers de totu le code
+const id = getUrlParameterId() //Stocker l'id dans une constante au travers de tout le code
 
 async function getProductFromId(id) {
   return fetch(`http://localhost:3000/api/products/${id}`) //Récupération du produit correspondant à id
@@ -43,7 +43,7 @@ async function createProductPage() {
 createProductPage()
 
 
-/* Lorsque je change à l'intérieur du select et que je choisis une option, le select.value prend la valeur de l'option sélectionnée.
+/*// Lorsque je change à l'intérieur du select et que je choisis une option, le select.value prend la valeur de l'option sélectionnée.
 async function addSelectedToColorOption() {
   await getProductFromId(id)  //Attendre d'obtenir la liste des options
   let selection = document.getElementById('colors') //Cibler la liste d'options
@@ -62,73 +62,151 @@ async function addSelectedToColorOption() {
 
 addSelectedToColorOption()*/
 
+function verifyColor() {
+  let colorSelector = document.getElementById('colors')
+  colorSelector.addEventListener('change', () => {
+    if (colorSelector.value === "" && !colorSelector.parentElement.querySelector('p')) {
+      colorSelector.removeAttribute('value')
+      let errorMessage = document.createElement('p')
+      errorMessage.innerText = "Veuillez choisir une couleur de produit pour l'ajouter au panier."
+      colorSelector.parentElement.appendChild(errorMessage)
+    } else if (colorSelector.value != "") {
+      if (colorSelector.parentElement.querySelector('p')) {
+        colorSelector.parentElement.removeChild(document.querySelector('.item__content__settings__color p:last-child'))
+      }
+    }
+  })
+  return colorSelector.value
+}
+verifyColor()
+
 function verifyQuantity() {
   let quantity = document.getElementById('quantity')  //Sélection de l'input quantity
   let errorMessage = document.createElement('p')  //Création d'un paragraphe contenant le futur message d'erreur
   quantity.addEventListener('change', () => {  //Fonction traitant les divers cas de valeurs saisies
     if (quantity.value == Number.isNaN(quantity.value) && quantity.value != "0") {  //Si les données saisies ne sont pas un nombre et suppression de la particularité du cas Zéro du comportement de isNaN
-      quantity.value = 0  //Remise de l'input à zéro
-      errorMessage.innerText = "Veuillez entrer un nombre s'il vous plait"  //Message d'erreur spécifique
+      quantity.value = 0  //Mise de l'input à la valeur par défaut
+      errorMessage.innerText = "Veuillez entrer un nombre s'il vous plait"  
+      if (!document.querySelector('.item__content__settings__quantity p')) {
       document.querySelector('.item__content__settings__quantity').appendChild(errorMessage)  //Insertion du message d'erreur
+    }
+    return console.error('not a number')
     } else if(quantity.value == "0" || Number(quantity.value) < 1) { //Traitement des cas où input <1 et traitement du cas spécifique ou le chiffre saisi est 0
-      quantity.value = 0  //Remise de l'input à zéro
-      errorMessage.innerText = "La quantité minimale à ajouter au panier est de un (1)" //Message d'erreur spécifique
-      document.querySelector('.item__content__settings__quantity').appendChild(errorMessage)  //Insertion du message d'erreur
+      quantity.value = 0  //Mise de l'input à la valeur par défaut
+      errorMessage.innerText = "La quantité minimale à ajouter au panier est de un (1)" 
+      if (!document.querySelector('.item__content__settings__quantity p')) {
+        document.querySelector('.item__content__settings__quantity').appendChild(errorMessage)  //Insertion du message d'erreur
+      }
+      return console.error('0 or less')
     } else if (Number(quantity.value) > 100) {  //Traitement du cas où la quantité demandée est supérieure à 100
-      quantity.value = 100  //Mise de l'input à la valeur maximale
-      document.getElementById('quantity').setAttribute('value', `${quantity.value}`)
-      errorMessage.innerText = "La quantité maximale est de cent (100)" //Message d'erreur sépcifique
-      document.querySelector('.item__content__settings__quantity').appendChild(errorMessage)  //Insertion du message d'erreur
+      quantity.value = 0 //Mise de l'input à la valeur par défaut
+      errorMessage.innerText = "La quantité maximale est de cent (100)" 
+      if (!document.querySelector('.item__content__settings__quantity p')){
+        document.querySelector('.item__content__settings__quantity').appendChild(errorMessage)  //Insertion du message d'erreur
+      }
+      return console.error('over 100')
     } else if ((Number(quantity.value) >= 1) && (Number(quantity.value) <= 100)) { //Cas où les données sont correctes
       document.getElementById('quantity').setAttribute('value', `${quantity.value}`)
       if(document.querySelector('.item__content__settings__quantity p:last-child')) { //Cas où les données sont corrigées de la part de l'utilisateur 
         document.querySelector('.item__content__settings__quantity').removeChild(document.querySelector('.item__content__settings__quantity p:last-child')) //Suppression du message d'erreur
       }
+      return quantity.value
     }
-    //quantity.innerText = event.target.value
-    return quantity.value
   })
+  return Number(quantity.value)
 }
 
 verifyQuantity()
 
-function createCommandProductInLocalStorage() {
-  let button = document.getElementById('addToCart')
-  button.addEventListener('click', () => {
-    let selector = document.getElementById('colors')
-    let colors = selector.querySelectorAll('option')
-    let numberOfColors = (colors.length - 1)
-    let color = selector.value
-    let quantityInput = document.getElementById('quantity')
-    let quantity = quantityInput.value
-    let idProduct = JSON.stringify(id)
-    let newColorProduct = {
-      'color': color,
-      'quantity': quantity
+function isColorInCart(array = [], colorToFind) {
+  for (let object of array) {
+    if(object.color == colorToFind) {
+      return true
     }
-    idProduct = new Array(newColorProduct)
-    console.log(idProduct)
-    //window.localStorage.setItem('cart', JSON.stringify(cart))
+  }
+  return false
+}
+
+
+
+
+function createCommandProductInLocalStorage() {
+  let button = document.getElementById('addToCart')  
+  button.addEventListener('click', () => {
+    let colorSelected = verifyColor()
+    let quantitySelected = verifyQuantity()
+    if ((colorSelected != "") && ((quantitySelected >= 1) && (quantitySelected <= 100))) {
+      if (!window.localStorage.getItem('cart')) {
+        let cart = {}
+        cart[id] = []
+        let newColorAdded = {
+          color: colorSelected,
+          quantity: quantitySelected
+        }
+        cart[id].push(newColorAdded)
+        window.localStorage.setItem('cart', JSON.stringify(cart))
+      } else if (window.localStorage.getItem('cart')) {
+        let cart = JSON.parse(window.localStorage.getItem('cart'))
+        if (!cart.hasOwnProperty(id)) {
+          cart[id] = []
+          let newColorAdded = {
+            color: colorSelected,
+            quantity: quantitySelected
+          }
+          cart[id].push(newColorAdded)
+          window.localStorage.setItem('cart', JSON.stringify(cart))
+        } else if (cart.hasOwnProperty(id)) {
+          let products = cart[id]
+          console.log(products)
+          if (isColorInCart(products, colorSelected)) {
+            for (let product of products) {
+              if (product.color === colorSelected) {
+                product.quantity = product.quantity + quantitySelected
+                window.localStorage.setItem('cart', JSON.stringify(cart))
+              }
+            }
+          } else {
+            let newColorAdded = {
+              color: colorSelected,
+              quantity: quantitySelected
+            }
+            cart[id].push(newColorAdded)
+            window.localStorage.setItem('cart', JSON.stringify(cart))
+          }
+        }    
+      }
+    }
+    if (document.querySelector('.item__content > p')) {
+      let divItemContent = document.querySelector('div.item__content')
+      divItemContent.removeChild(document.querySelector('div.item__content > p:last-child'))
+    } else {
+    let addErrorMessage = document.createElement('p')
+    addErrorMessage.innerText = "Veuillez vérifier la couleur et la quantité sélectionnées"
+    addErrorMessage.setAttribute('style', 'text-align: center')
+      if (!document.querySelector('.item__content > p')) {
+        document.querySelector('.item__content').appendChild(addErrorMessage)
+      }
+    }
   })
 }
 
 createCommandProductInLocalStorage()
 
-/*
-function getColorOption() {
-  let option = document.getElementById('colors').value
-  return option
-}
 
-function getNumberOfArticle() {
-  let number = document.getElementById('quantity').value
-
-}
-
-async function createQueryString() {
-  await getProductFromId()
-  let colorPicked = getColorOption()
-  console.log(colorPicked)
-}
-
-createQueryString() */
+    /*let selector = document.getElementById('colors')
+    let colors = selector.querySelectorAll('option')
+    let numberOfColors = (colors.length - 1)
+    let color = selector.value
+    let quantityInput = document.getElementById('quantity')
+    let quantity = quantityInput.value
+    let errorMessage = document.createElement('p')
+    if (selector.value === "" && errorMessage.innerText != "") {
+      errorMessage.innerText = "Veuillez choisir une couleur de produit pour l'ajouter au panier."
+      let colorParent = document.querySelector('.item__content__settings__color')
+      colorParent.appendChild(errorMessage)
+      if (Number(quantity) < 1 || Number(quantity > 100)) {
+        errorMessage = errorMessage + " Veuillez également entrer une quantité pour le produit."
+      }
+    }
+  })
+}*/
